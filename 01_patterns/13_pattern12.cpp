df581ee4570c9036e9937899c96e1eb11ebda4ba
Utkarsh Agrawal <agrawalutkarsh4701@gmail.com>
2025-04-13 00:39:57 +0530

    patterns

---
+++ 01_patterns/13_pattern12.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+// *             *
+// * *         * *
+// * * *     * * *
+// * * * * * * * *
+// * * * * * * * *
+// * * *     * * *
+// * *         * *
+// *             *
+
+int main(){
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout<<"* ";
+        }
+
+        for(int k=0; k< 2*(n-i-1); k++){
+            cout << "  ";
+        }
+
+        for(int l=0; l<=i; l++){
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=n; j>i; j--){
+            cout << "* ";
+        }
+
+        for(int k=0; k<2*i; k++){
+            cout<<"  ";
+        }
+
+        for(int l=0; l<n-i; l++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
